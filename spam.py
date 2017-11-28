e89e47d7a1c6022dcf952fc9001ac006d267a721
Tushar <tushar.169107181@muj.manipal.edu>
2017-11-29 01:16:46 +0530

    analysis of dataset done correctly

--- spam.py
+++ spam.py
@@ -11,6 +11,20 @@ import pandas as pd
 import numpy as np
 
 df = pd.read_csv("spam.csv")
+x=df.iloc[:,:].values
+countham=0
+countspam=0
+for i in range(len(x)):
+    if x[i][0]=='ham':
+        countham+=1
+        
+    else:
+        countspam+=1
+        
+        
+
+
+
 
 from sklearn.model_selection import train_test_split
 
@@ -21,6 +35,25 @@ data_train, data_test, labels_train, labels_test = train_test_split(
     test_size=0.2, 
     random_state=0) 
 
+x=labels_train.iloc[:].values
+ltrainham=0
+ltrainspam=0
+for i in range(len(x)):
+    if x[i]=='ham':
+        ltrainham+=1
+        
+    else:
+        ltrainspam+=1
+
+x=labels_test.iloc[:].values
+ltrainham=0
+ltrainspam=0
+for i in range(len(x)):
+    if x[i]=='ham':
+        ltrainham+=1
+        
+    else:
+        ltrainspam+=1
 print (data_train.head())
 print (labels_train.head())
 
@@ -52,7 +85,7 @@ y_pred2 = classifier.predict(data_test_count)
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(labels_test,y_pred2)
 
-sc = classifier.score(data_test_count,labels_test)
+sclog = classifier.score(data_test_count,labels_test)
 
     
 check = pd.DataFrame(labels_test)
@@ -68,7 +101,7 @@ check3 = pd.DataFrame(y_pred3)
 
 cm1 = confusion_matrix(labels_test,y_pred3)
 
-sc1 = classifier1.score(data_test_count,labels_test)
+scKN = classifier1.score(data_test_count,labels_test)
 
 from sklearn.tree import DecisionTreeClassifier
 classifier2 = DecisionTreeClassifier(criterion = 'entropy', random_state =0)
@@ -79,7 +112,7 @@ y_pred4 = classifier2.predict(data_test_count)
 check4 = pd.DataFrame(y_pred4)
 
 cm2 = confusion_matrix(labels_test,y_pred4)
-sc2 = classifier2.score(data_test_count,labels_test)
+scDT = classifier2.score(data_test_count,labels_test)
 
 
 from sklearn.ensemble import RandomForestClassifier
@@ -91,5 +124,5 @@ y_pred5 = classifier3.predict(data_test_count)
 check5 = pd.DataFrame(y_pred5)
 
 cm3 = confusion_matrix(labels_test,y_pred4)
-sc3 = classifier3.score(data_test_count,labels_test)
+scRF = classifier3.score(data_test_count,labels_test)
 
